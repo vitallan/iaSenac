ab24524ed7676918859395013e0c4102e45c234b
Fabrício J. Barth <fabricio.barth@gmail.com>
2014-09-16 22:39:59 -0300

    Excluindo um projeto desatualizado

--- ExemplosDrools/src/main/java/example/health/Tipos.java
+++
@@ -1,9 +0,0 @@
-package example.health;
-
-public class Tipos {
-	
-	public enum CADASTRO {OK, NAO_OK};
-	public enum CARENCIA {DENTRO_LIMITE,FORA_LIMITE};
-	public enum SINISTRO {ACIDENTE,PARTO,EXAMES_NORMAIS};
-
-}
